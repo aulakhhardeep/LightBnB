1b1c28841e65a5dff72c4d3b305ea5e1bb7a5179
Hardeep Kaur Aulakh <aulakh.hardeep91@gmail.com>
2023-07-09 19:53:08 -0600

    Refractor getAllReservations function

--- LightBnB_WebApp-master/db/database.js
+++ LightBnB_WebApp-master/db/database.js
@@ -86,9 +86,37 @@ const addUser = (user) => {
  * @param {string} guest_id The id of the user.
  * @return {Promise<[{}]>} A promise to the reservations.
  */
-const getAllReservations = function (guest_id, limit = 10) {
-  return getAllProperties(null, 2);
+/**
+ * Get all reservations for a single user.
+ * @param {string} guest_id The id of the user.
+ * @param {number} limit The maximum number of reservations to retrieve.
+ * @return {Promise<[{}]>} A promise to the reservations.
+ */
+
+
+const getAllReservations = (guest_id) => {
+  const limit = 10;
+  return pool
+    .query(
+      `SELECT reservations.id, properties.title, properties.cost_per_night, reservations.start_date, AVG(property_reviews.rating) AS average_rating
+       FROM reservations
+       JOIN properties ON reservations.property_id = properties.id
+       JOIN property_reviews ON properties.id = property_reviews.property_id
+       WHERE reservations.guest_id = $1
+       GROUP BY properties.id, reservations.id
+       ORDER BY reservations.start_date
+       LIMIT $2`,
+      [guest_id, limit]
+    )
+    .then((result) => {
+      return result.rows;
+    })
+    .catch((err) => {
+      console.log(err.message);
+      throw err; // Propagate the error to the caller
+    });
 };
+
 
 /// Properties
 
